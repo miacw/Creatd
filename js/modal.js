261c51001b47af13f7d2d55452b566659d1220cf
miacw <38225032+miacw@users.noreply.github.com>
2022-04-12 22:00:20 +0100

    Add files via upload

--- js/modal.js
+++ js/modal.js
@@ -3,18 +3,27 @@ let modal = document.querySelector(".main-modal");
 let modalImg = document.querySelector(".modal-img");
 let body = document.querySelector("body");
 
-modalBtnEls.forEach((modalBtn) => {
-  modalBtn.addEventListener("click", function () {
-    let thumbnail =
-      this.parentElement.parentElement.previousElementSibling.getAttribute(
-        "src"
-      );
+if (window.screen.width > 580) {
+  modalBtnEls.forEach((modalBtn) => {
+    if ((modalBtn.disabled = true)) {
+      modalBtn.removeAttribute("disabled");
+    }
+    modalBtn.addEventListener("click", function () {
+      let thumbnail =
+        this.parentElement.parentElement.previousElementSibling.getAttribute(
+          "src"
+        );
 
-    modalImg.src = `${thumbnail}`;
-    modal.classList.add("flex");
-    body.classList.add("stop-scroll");
+      modalImg.src = `${thumbnail}`;
+      modal.classList.add("flex");
+      body.classList.add("stop-scroll");
+    });
   });
-});
+} else {
+  modalBtnEls.forEach((modalBtn) => {
+    modalBtn.disabled = true;
+  });
+}
 
 modal.addEventListener("click", function () {
   modal.classList.remove("flex");
